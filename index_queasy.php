48d8fa086e74e9d2aa39ce3a0e8aab719a5c309a
FloVth <florianvauthier70@gmail.com>
2023-03-07 16:33:46 +0100

    accueil

--- index_queasy.php
+++ index_queasy.php
@@ -2,7 +2,7 @@
 <html>
     <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-      <link href="assets/css/style.css" rel="stylesheet" >
+      <link href="assets/style.css" rel="stylesheet" >
       <link href="Polices/VisbyRoundCF-Heavy.otf" rel="stylesheet" type="text/css">
       <link rel="shortcut icon" href="Logo-QUEASY.png"/>
     </head>
