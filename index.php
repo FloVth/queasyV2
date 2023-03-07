56f484b14f1f68a006a6034dcc3c0e099d4dc700
FloVth <florianvauthier70@gmail.com>
2023-03-07 15:41:27 +0100

    modif

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html>
     <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-      <link href="assets/style.css" rel="stylesheet" >
+      <link href="assets/css/style.css" rel="stylesheet" >
+      <link href="Polices/VisbyRoundCF-Heavy.otf" rel="stylesheet" type="text/css">
+      <link rel="shortcut icon" href="Logo-QUEASY.png"/>
     </head>
     <body>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <div class="responsive-image">
+                <img src="assets/image/Logo-QUEASY.png" alt="Logo du QUEASY" class="responsive-image" width="250" height="250">
+        </div>
 <?php
 /*include("config/database.php");
 if (isset($_SESSION["error"])){
