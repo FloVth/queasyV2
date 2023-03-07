dd09829c23f89d72e4e13437386ef1098a0df64f
FloVth <florianvauthier70@gmail.com>
2023-03-07 14:41:11 +0100

    creation du projet avec archi propre

---
+++ page/auth/login.php
@@ -0,0 +1,32 @@
+<div class="Votre_Espace">Connectez-vous ! </div>
+<!--Login-->
+<form>
+    <!--LABEL NOM-->
+    <div class="wrappper">
+        <div class="content">
+            <div class="box">
+                <label>Nom :</label>
+            </div>
+
+    <!--LOGIN INPUT -->
+    <div class="box">
+        <input type="text" class="myinput" name="Nom" placeholder="Entrez votre nom" required>
+    </div>
+
+    <!--LOGIN MOT DE PASSE-->
+    <div class="box">
+        <label>mot de passe :</label>
+    </div>
+
+    <!--INPUT MOT DE PASSE-->
+    <div class="box">
+        <input type="password" class="myinput" name="mot de passe :"  placeholder="Entrez votre mot de passe" required>
+    </div>
+
+    <!--Bouton-->
+    <div class="button">
+        <div class="box">
+        <a href="check_login.php" class='btn'>Accéder à mon profil ! </a>
+        </div>
+    </div>
+</form>     
