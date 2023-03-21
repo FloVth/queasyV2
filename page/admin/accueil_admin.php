a11f8d7cac38a61ddadc1858afce176c70127a8b
FloVth <florianvauthier70@gmail.com>
2023-03-21 17:25:28 +0100

    findelaseance

--- page/admin/accueil_admin.php
+++ page/admin/accueil_admin.php
@@ -1,24 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <linK rel="stylesheet" href="espace_eleve.css"> 
-        <link href="Polices/VisbyRoundCF-Heavy.otf" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
-        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <title>QUEASY | Espace Admin</title>
     </head>
     <body>
          <!-- ajout de la zone de l'utilisateur -->
-        <?php include 'headerutilisateur.php'; ?>
-            
- 
-        <!-- logo du queasy -->
-        <div class="Logo-QUEASY">
-            <img src="Logo-QUEASY.png" alt="Logo du QUEASY" class="responsive-image" width="250" height="250">
-        </div>
+        <?php include 'page/header.php' ?>
+
         <!--TEXT VOTRE ESPACE-->
         <div class="Votre_Espace">Votre espace</div>
         <!--Zone espace-->
@@ -39,7 +27,7 @@
         <div class="wrapper">
             <div class="content">
                 <div class="box">
-                    <img src="adduser.png">
+                    <img src="assets/image/adduser.png">
                     <h2>Gérer des élèves</h2>
                     <p>Gérer un groupe de personnes ainsi que leurs promotions, afin de leur envoyer rapidement.</p>
                     <a href='gerereleve.php' class="btn"> Ajouter ! <i class="fa-solid fa-user-plus"></i>
@@ -48,14 +36,14 @@
                 </div>
         <!--ZONE DE RESULTAT-->
                 <div class="box">
-                    <img src="Trophy.png">
+                    <img src="assets/image/Trophy.png">
                     <h2>Résultat</h2>
                     <p>Avec cette fonctionnalité, vous pourrez désormais voir le score des personnes ayant effectué votre quizz.</p>
                     <a href='/QUEASY/https---github.com-FloVth-Queasy/classement.php' class="btn">voir les résultats ! <i class="fa-solid fa-trophy"></i> </a>
                 </div>
                  <!--ZONE DE NOUVEAU QUIZZ-->
                 <div class="box">
-                    <img src="plus.png">
+                    <img src="assests/image/plus.png">
                     <h2>Gérer un nouveau Quizz</h2>
                     <p>Gérer maintenant votre propre quizz 20 questions à créer vous attend.</p>
                     <a href='creerqueasy.php' class="btn"> Créer mon QUEASY ! <i class="fa-sharp fa-solid fa-circle-plus"></i></a>
---
+++ page/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head> <!-- Il faut préciser "#user" afin d'éviter de changer les autres éléments sur les autres pages-->
+	<style>
+		#user {
+			float: right;
+			margin-right: 20px;
+			margin-top: 10px;
+		}	
+
+        		#user p {
+			float: right;
+            margin-top: 15px;
+			font-size: 2em;
+		}
+		
+		#user img 
+		{
+			width= 40px height= 50px
+		}
+		#user a { 
+			text-decoration: none;  
+			color: inherit;  
+			display: block; 
+			height: 100%; 
+		}
+
+		#retour {
+		float: right;
+		margin-right: 20px;
+		margin-top: 10px;
+	}
+	
+	#retour a {
+		text-decoration: none;
+		background-color: #03224C;
+		color: white;
+		padding: 10px 20px;
+		border-radius: 5px;
+	}
+
+		
+	</style>
+</head>
+<body>
+    <?php
+    $user_name = $_SESSION["login"];
+    $user_icon = "assets/image/user.png";
+     ?>
+	<header>
+		<div id="user">
+			<img src="<?php echo $user_icon; ?>" alt="Icône utilisateur">
+			<p><a href="compte utilisateur.php"><?php echo $user_name; ?></a></p>
+</div>
+		
+<div id="retour">
+	<br>
+			<a href="Espace admin.php">Retour au menu principale</a>
+		</div>
+
+	</header>
+</body>
+</html>
