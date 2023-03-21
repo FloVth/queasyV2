c9b2e2be9d28739205ea8b25e64a7d4145bddc05
EthanDeL <ethandelbos@orange.fr>
2023-03-21 16:39:43 +0100

    ajout de alt

--- page/eleve/accueil_eleve.php
+++ page/eleve/accueil_eleve.php
@@ -23,19 +23,26 @@
                 <p class="profil">Nom</p>
                 <p class="profil">Classe</p>
             </div>
+            <p>
+                <?php
+                    echo $_SESSION["login"]; 
+                    // echo $_SESSION["premon_user"]; 
+                    // echo $_SESSION["id_promotion"];
+                ?>
+            </p>
         </div>
         <!--ZONE CORRECTION-->
         <div class="wrapper">
             <div class="content">
                 <div class="box">
-                    <img src="assets/image/Check.png">
+                    <img src="assets/image/Check.png" alt="Pictogramme check">
                     <h2>Correction</h2>
                     <p>Consulter les corrections de vos QUEASY pour apprendre de vos erreurs.</p>
                     <a href='#' class="btn"> voir les corrections ! <i class="fa-regular fa-circle-check"></i></a>
                 </div>
         <!--ZONE DE RESULTAT-->
                 <div class="box">
-                    <img src="assets/image/Trophy.png">
+                    <img src="assets/image/Trophy.png" alt="Pictogramme trophy">
                     <h2>Résultat</h2>
                     <p>Regarder vos scores et le classement de votre QUEASY.</p>
                     <a href='#' class="btn">voir les résultats ! <i class="fa-solid fa-trophy"></i> </a>
