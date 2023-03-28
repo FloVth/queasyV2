5c11ac106105818148a5d6c6c66542872db1a132
EthanDeL <ethandelbos@orange.fr>
2023-03-28 15:21:10 +0200

    Mise a jour de l'ajout d'un bouton + lien vers pag

--- page/eleve/Correction_QUEASY.php
+++ page/eleve/Correction_QUEASY.php
@@ -7,6 +7,27 @@
         <title>QUEASY | Correction</title>
     </head>
     <body>
-        
+        <!--ZONE TITRE-->
+        <div class="Votre_Espace">Correction des QUEASY</div>
+
+        <!--ZONE ESPACE-->
+        <div class="zone-espace">
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+            <div class="spacing"></div>
+        </div>
+
+        <!--TEXT SOUS-TITRE-->
+        <div class="Titre_annonce">Regarder désormais la correction de vos QUEASY que vous avez réalisé ! </div>
+
+        <!--NOM DU QUEASY-->
+
+
+        <!--ZONE BOUTON-->
+
+        <!--BOUTON HOME-->
+        <a href='page/eleve/acceuil_eleve.php' class="btn">Menu ! <i class="fa-solid fa-house-chimney"></i> </a>
     </body>
 </html>
