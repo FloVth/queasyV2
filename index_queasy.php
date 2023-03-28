2ad01607834df3ecf4b5a68d4be22a0433d2f4c0
EthanDeL <ethandelbos@orange.fr>
2023-03-28 14:40:08 +0200

    Page deconnexion operationel

--- index_queasy.php
+++ index_queasy.php
@@ -60,12 +60,10 @@ if (isset($_SESSION["success"])){
             case "accueil_admin":
               include("page/admin/accueil_admin.php");
               break;
-            case "creerqueasy":
-              include ("page/admin/creerqueasy.php");
-              break;
-            case "gerer_eleve":
-              include ("page/admin/gerereleve.php");
-              break;
+            
+              case "Deconnexion":
+                include("page/auth/Deconnexion.php");
+                break;
                                                  
             default :
             if(isset($_SESSION["login"])){
