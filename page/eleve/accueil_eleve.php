acad2e260f94589691c7fcb9e67a92ee115f631b
EthanDeL <ethandelbos@orange.fr>
2023-03-28 13:41:52 +0200

    Mise en place edit user

--- page/eleve/accueil_eleve.php
+++ page/eleve/accueil_eleve.php
@@ -15,9 +15,24 @@
             <div class="spacing"></div>
             <div class="spacing"></div>
         </div>
+        <!--IMAGE CLIQUABLE-->
+        <div class="edit_profil">
+            <img class="Image_Profil" src="assets/image/pictogramme_edit.png" onclick="Profil()" alt='pictogramme edit profil' >
+                <div id="zone_profil" class="edit_profil-content">
+                  <img class="edit_img" src="assets/image/pictogramme_edit.png" alt='pictogramme edit profil'> <!--sera remplacer par des vrai pdp-->
+                    <p class="edit_user">Modifie ta photo de profil</p>
+                    <p class="edit_user"><?php echo $_SESSION["login"]?></p>
+                    <p class="edit_user"><?php echo $_SESSION["prenom"]?></p>
+                    <p class="edit_user"><?php echo $_SESSION["promo"]?></p>
+                    <div class="spacing"></div>
+                    <div class="spacing"></div>
+                    <div class="spacing"></div>
+                    <a href="#" class="edit">Modifie ton mot de passe<a>
+                    <a href="#" class="edit">Déconnexion<a>
+                </div>
+        </div>
         <!--ZONE DU PROFIL-->
         <div class="contenue_profil">
-                <img class="profil" src="assets/image/pictogramme_edit.png" alt='pictogramme edit profil'>
                 <p class="profil"><?php echo $_SESSION["login"]?></p>
                 <p class="profil"><?php echo $_SESSION["prenom"]?></p>
                 <p class="profil"><?php echo $_SESSION["promo"]?></p>
@@ -31,7 +46,6 @@
                     <p>Consulter les corrections de vos QUEASY pour apprendre de vos erreurs.</p>
                     <a href='#' class="btn"> voir les corrections ! <i class="fa-regular fa-circle-check"></i></a>
                 </div>
-
         <!--ZONE REJOINDRE-->
                 <div class="box">
                     <img src="" alt="Pictogramme communauty">
@@ -49,5 +63,26 @@
                 </div>
             </div>
         </div>
+
+        <!--SCRIPT ZONE EDIT USER-->
+        <script>
+        // Si utilisateur clique sur la photo de profil alors affiche de l'edit menu
+        function Profil() {
+          document.getElementById("zone_profil").classList.toggle("montrer");
+        }
+        // Ferme l'edit menu s'il click ailleur que l'image de profil
+        window.onclick = function(event) {
+          if (!event.target.matches('.Image_Profil')) {
+            var edit_profils = document.getElementsByClassName("edit_profil-content"); // On récupere les valeurs se trouvant dans "edit_profil_content"
+            var i;
+            for (i = 0; i < edit_profils.length; i++) {
+              var ouvre_edit_profil = edit_profils[i];
+              if (ouvre_edit_profil.classList.contains('montrer')) {
+                ouvre_edit_profil.classList.remove('montrer');
+              }
+            }
+          }
+        }
+        </script>     
     </body>
 </html>
