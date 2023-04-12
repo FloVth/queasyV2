2125d465337cde1e3d58cf9c7a1c6f696262cd60
EthanDeL <ethandelbos@orange.fr>
2023-04-12 16:46:54 +0200

    Remise au point

--- page/auth/edit_password.php
+++ page/auth/edit_password.php
@@ -1,62 +1,38 @@
 <!--Login-->
 <form action="index_queasy.php?route=edit_password" method="post">
 
-    <!--LABEL MOT DE PASSE ACTUEL-->
+    <!--LABEL ANCIEN MOT DE PASSE-->
     <div class="wrappper">
         <div class="content">
             <div class="box">
-                <label for="mot_pde_passe_actuel">Mot de passe actuel : </label>
+                <label for="ancien_mot_de_passe">Ancien mot de passe :</label>
             </div>
         </div>
-        <!-- INPUT MOT DE PASSE ACTUEL-->
+        <!--INPUT ANCIEN MOT DE PASSE-->
         <div class="box">
-            <input type="text" class="myinput" id="mot_de_passe_actuel" placeholder="Entrez votre mot passe " required/>
-            <span id="mot_de_passe_actuel_erreur" class="cache couleur_obligation_message_erreur">Votre mot de passe n'est pas le même</span>
-            <span id="mot_de_passe_actuel_vide" class="cache couleur_obligation_message_erreur">Saisie invalide</span>
+            <input type="text" class="myinput" name="ancien_mot_de_passe" id="ancien_mot_de_passe" placeholder="Entrez votre nom" required/>
         </div>
 
-        <!--ESPACE-->
-        <div class="zone-espace">
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-        </div>
 
-        <!--LOGIN NOUVEAU MOT DE PASSE-->
+        <!--LABEL NOUVEAU MOT DE PASSE-->
         <div class="box">
-            <label for="nouveau_mot_de_passe"> Nouveau mot de passe :</label>
+            <label for="nouveau_mot_de_passe">Nouveau mot de passe :</label>
         </div>
         <!--INPUT NOUVEAU MOT DE PASSE-->
         <div class="box">
-            <input type="password" class="myinput" id="nouveau_mot_de_passe"  placeholder="Nouveau mot de passe" required/>
-            <span id="nouveau_mot_de_passe_erreur" class="cache couleur_obligation_message_erreur">Votre mot de passe doit contenir des lettres et des chiffres</span>
-            <span id="nouveau_mot_de_passe_vide" class="cache couleur_obligation_message_erreur">Saisie invalide</span>
+            <input type="password" class="myinput" name="nouveau_mot_de_passe" id="nouveau_mot_de_passe"  placeholder="Nouveau mot de passe" required/>
         </div>
 
-        <!--ESPACE-->
-        <div class="zone-espace">
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-        </div>
 
-        <!--LOGIN CONFIRMATION MOT DE PASSE-->
-        <div class="box">
-            <label for="confirmation_mot_de_passe"> Confirmation mot de passe :</label>
+        <!--LABEL CONFIRMATION MOT DE PASSE-->
+         <div class="box">
+            <label for="confirmation_mot_de_passe">confirmation du nouveau mot de passe :</label>
         </div>
         <!--INPUT CONFIRMATION MOT DE PASSE-->
         <div class="box">
-            <input type="password" class="myinput" id="confirmation_mot_de_passe"  placeholder="Nouveau mot de passe" required/>
-            <span id="confirmation_mot_de_passe_erreur" class="cache couleur_obligation_message_erreur">Votre mot de passe n'est pas le même que celui saisie précedement</span>
-            <span id="confirmation_mot_de_passe_vide" class="cache couleur_obligation_message_erreur">Saisie invalide</span>
+            <input type="password" class="myinput" name="confirmation_mot_de_passe" id="confirmation_mot_de_passe"  placeholder="Confirmation du mot de passe" required>
         </div>
 
-        <!--ESPACE-->
-        <div class="zone-espace">
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-        </div>
 
         <!--Bouton-->
         <div class="box">
