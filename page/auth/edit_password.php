ae970ec740a81caeb9f910f2c0bfc04440dd5c61
EthanDeL <ethandelbos@orange.fr>
2023-04-14 09:57:15 +0200

    Debut PHP changer de mot de passe

--- page/auth/edit_password.php
+++ page/auth/edit_password.php
@@ -1,66 +1,82 @@
-<!--Login-->
-<form action="index_queasy.php?route=edit_password" method="post">
-    <?php
-        include("config/database.php");
-        if(isset($_POST['sumbit'])){
-            $ancien_mot_de_passe = $_POST['ancien_mot_de_passe'];
-            $nouveau_mot_de_passe = $_POST['nouveau_mot_de_passe'];
-            $confirmation_mot_de_passe = $_POST['confirmation_mot_de_passe']
-        }
+    <?php 
+    seesion_start();
 
-    $requete = $mysqlConnection->prepare('SELECT * FROM user where nom_user = :login and mdp_user=:password');$
-    
+    if (isset($_SESSION['id_user'])&& isset($_SESSION['nom_user'])){
     ?>
-    <!--LABEL ANCIEN MOT DE PASSE-->
-    <div class="wrappper">
-        <div class="content">
+
+
+    <form action ="edit-password.php" method="post">
+        <!--Titre page-->
+        <h2>Changer votre mot de passe</h2>
+        <?php if(isset($_GET[('error')])) {?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?> 
+
+        <?php if(isset($_GET['succes'])){ ?>
+            <p class="succes"><?php echo $_GET ['succes']; ?></p>
+        <?php } ?>
+
+        <!--LABEL ANCIEN MOT DE PASSE-->
+        <div class="wrappper">
+            <div class="content">
+                <div class="box">
+                    <label for="ancien_mot_de_passe">Ancien mot de passe :</label>
+                </div>
+            </div>
+            <!--INPUT ANCIEN MOT DE PASSE-->
             <div class="box">
-                <label for="ancien_mot_de_passe">Ancien mot de passe :</label>
+                <input type="text" class="myinput" name="ancien_mot_de_passe" placeholder="Entrez votre nom"/>
+            </div>
+
+            <!--ZONE ESPACE-->
+            <div class="zone-espace">
+                <div class="spacing"></div>
+                <div class="spacing"></div>
+            </div>
+
+
+            <!--LABEL NOUVEAU MOT DE PASSE-->
+            <div class="box">
+                <label for="nouveau_mot_de_passe">Nouveau mot de passe :</label>
+            </div>
+            <!--INPUT NOUVEAU MOT DE PASSE-->
+            <div class="box">
+                <input type="password" class="myinput" name="nouveau_mot_de_passe" placeholder="Nouveau mot de passe"/>
+            </div>
+
+            <!--ZONE ESPACE-->
+            <div class="zone-espace">
+                <div class="spacing"></div>
+                <div class="spacing"></div>
+            </div>
+
+
+            <!--LABEL CONFIRMATION MOT DE PASSE-->
+            <div class="box">
+                <label for="confirmation_mot_de_passe">confirmation du nouveau mot de passe :</label>
+            </div>
+            <!--INPUT CONFIRMATION MOT DE PASSE-->
+            <div class="box">
+                <input type="password" class="myinput" name="confirmation_mot_de_passe" placeholder="Confirmation du mot de passe"/>
+            </div>
+
+            <!--ZONE ESPACE-->
+                <div class="zone-espace">
+                <div class="spacing"></div>
+                <div class="spacing"></div>
+            </div>
+
+            <!--Bouton-->
+            <div class="box">
+                <div class="button">
+                <a href='index_queasy.php?route=accueil_eleve' class="btn">Changer de mot de passe </a>
+                <a href='index_queasy.php?route=accueil_eleve' class="btn">Menu ! <i class="fa-solid fa-house-chimney"></i> </a>
+                </div>
+                
             </div>
         </div>
-        <!--INPUT ANCIEN MOT DE PASSE-->
-        <div class="box">
-            <input type="text" class="myinput" name="ancien_mot_de_passe" id="ancien_mot_de_passe" placeholder="Entrez votre nom" required/>
-        </div>
-
-        <!--ZONE ESPACE-->
-        <div class="zone-espace">
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-        </div>
-
-
-        <!--LABEL NOUVEAU MOT DE PASSE-->
-        <div class="box">
-            <label for="nouveau_mot_de_passe">Nouveau mot de passe :</label>
-        </div>
-        <!--INPUT NOUVEAU MOT DE PASSE-->
-        <div class="box">
-            <input type="password" class="myinput" name="nouveau_mot_de_passe" id="nouveau_mot_de_passe"  placeholder="Nouveau mot de passe" required/>
-        </div>
-
-        <!--ZONE ESPACE-->
-        <div class="zone-espace">
-            <div class="spacing"></div>
-            <div class="spacing"></div>
-        </div>
-
-
-        <!--LABEL CONFIRMATION MOT DE PASSE-->
-         <div class="box">
-            <label for="confirmation_mot_de_passe">confirmation du nouveau mot de passe :</label>
-        </div>
-        <!--INPUT CONFIRMATION MOT DE PASSE-->
-        <div class="box">
-            <input type="password" class="myinput" name="confirmation_mot_de_passe" id="confirmation_mot_de_passe"  placeholder="Confirmation du mot de passe" required>
-        </div>
-
-
-        <!--Bouton-->
-        <div class="box">
-            <div class="button">
-            <button type="submit" name="submit" class='btn'>Accéder à mon profil ! </button>
-            </div>
-        </div>
-    </div>
-</form>
+    </form>
+<?php 
+}else{
+    header("location:index_queasy.php?route=login");
+}?>
