c9764cb263e88562e07c9313e37251a26063224c
HotDogNoir <hotdognoir@yahoo.fr>
2023-04-25 16:57:43 +0200

    CSV s'est creer et suppression de code inutile

--- page/admin/afficher_quizz.php
+++ page/admin/afficher_quizz.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <linK rel="stylesheet" href="espace_eleve.css"> 
-        <link href="Polices/VisbyRoundCF-Heavy.otf" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
-        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <!--favicon-->
+
         <link rel="shortcut icon" href="Logo-QUEASY.png"/>
         <title>QUEASY | Espace Admin</title>
 
